c683f3a62f8fe55cc7bf7b9fdb26b1222481485f
oboehm <o.boehm@optica.de>
2018-11-08 22:13:32 +0100

    ResourcepathMonitor is derived now from clazzfish's ResourcepathMonitor

--- patterntesting-rt/src/main/java/patterntesting/runtime/monitor/ResourcepathMonitor.java
+++ patterntesting-rt/src/main/java/patterntesting/runtime/monitor/ResourcepathMonitor.java
@@ -20,21 +20,7 @@
 
 package patterntesting.runtime.monitor;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import patterntesting.runtime.jmx.Description;
-import patterntesting.runtime.monitor.internal.DoubletDigger;
-import patterntesting.runtime.monitor.internal.ResourcepathDigger;
-import patterntesting.runtime.util.ArchivEntry;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
 import java.net.URI;
-import java.net.URL;
-import java.util.*;
-import java.util.concurrent.*;
 
 /**
  * Analogous to ClasspathMonitor this class allows you to ask for resources in
@@ -44,14 +30,9 @@ import java.util.concurrent.*;
  * @version $Revision: 1.39 $
  * @since 1.6.4
  */
-public class ResourcepathMonitor extends clazzfish.monitor.AbstractMonitor implements clazzfish.monitor.ResourcepathMonitorMBean {
+public class ResourcepathMonitor extends clazzfish.monitor.ResourcepathMonitor {
 
-	private static final Logger LOG = LogManager.getLogger(ResourcepathMonitor.class);
-	private static final Executor EXECUTOR = Executors.newCachedThreadPool();
-	private static final ResourcepathMonitor INSTANCE;
-	private final ResourcepathDigger resourcepathDigger;
-	private final DoubletDigger doubletDigger;
-	private final FutureTask<String[]> resources;
+    private static final ResourcepathMonitor INSTANCE;
 
 	static {
 		INSTANCE = new ResourcepathMonitor();
@@ -62,21 +43,7 @@ public class ResourcepathMonitor extends clazzfish.monitor.AbstractMonitor imple
 	 * outside.
 	 */
 	private ResourcepathMonitor() {
-		this.resourcepathDigger = new ResourcepathDigger();
-		this.doubletDigger = new DoubletDigger(this.resourcepathDigger);
-		this.resources = getFutureResources();
-	}
-
-	private FutureTask<String[]> getFutureResources() {
-		Callable<String[]> callable = new Callable<String[]>() {
-			@Override
-			public String[] call() throws Exception {
-				return getResourcesArray();
-			}
-		};
-		FutureTask<String[]> resources = new FutureTask<>(callable);
-		EXECUTOR.execute(resources);
-		return resources;
+		super();
 	}
 
 	/**
@@ -119,221 +86,7 @@ public class ResourcepathMonitor extends clazzfish.monitor.AbstractMonitor imple
     public static boolean isRegisteredAsMBean() {
         return getInstance().isMBean();
     }
-    
-	/**
-	 * Scans the classpath for all resources. For performance reason we return
-	 * no longer a copy but the origin array. Don't do changes for the returned
-	 * array!
-	 *
-	 * @return all resources as String array
-	 */
-	@Override
-	public String[] getResources() {
-		try {
-			return this.resources.get();
-		} catch (ExecutionException ex) {
-			LOG.warn("Cannot execute get of {}:", this.resources, ex);
-		} catch (InterruptedException ex) {
-			LOG.warn("Was interrupted before got result from {}:", this.resources, ex);
-			Thread.currentThread().interrupt();
-		}
-		return this.getResourcesArray();
-	}
 
-	private String[] getResourcesArray() {
-		Set<String> resourceSet = this.resourcepathDigger.getResources();
-		return resourceSet.toArray(new String[resourceSet.size()]);
-	}
-
-	/**
-	 * Looks if the given resource can be found in the classpath.
-	 *
-	 * @param name
-	 *            of a resource e.g. "log4j.properties"
-	 * @return URI of the given resource (or null if resource was not found)
-	 */
-	@Override
-	@Description("returns the URI of the given resource")
-	public URI whichResource(final String name) {
-		return resourcepathDigger.whichResource(name);
-	}
-
-	/**
-	 * Gets the resources for a given name.
-	 *
-	 * @param name
-	 *            the name
-	 * @return the resources as URL enumeration
-	 */
-	public Enumeration<URL> getResourcesFor(final String name) {
-		return this.resourcepathDigger.getResources(name);
-	}
-
-	/**
-	 * Gets the number of resources.
-	 *
-	 * @param name the name of the resource
-	 * @return number of resources
-	 */
-	@Override
-	public int getNoResources(final String name) {
-		Enumeration<URL> urls = getResourcesFor(name);
-		if (urls == null) {
-			return 0;
-		}
-		int n = 0;
-		while (urls.hasMoreElements()) {
-			n++;
-			urls.nextElement();
-		}
-		LOG.trace("{} element(s) of {} found in classpath.", n, name);
-		return n;
-	}
-
-	/**
-     * Is the given classname or resource a doublet, e.g. can it be found
-     * several times in the classpath? If the classname or resource is not in
-     * the classpath a java.util.NoSuchElementException will be thrown.
-     *
-     * @param name a classname or resource
-     * @return true if more than one classname or resource was found in the
-     *         classpath
-     * @throws NoSuchElementException the no such element exception
-     * @throws java.util.NoSuchElementException if no classname or resource was
-     *         found
-     */
-	@Override
-	@Description("is the given classname or resource found more than once in the classpath?")
-	public boolean isDoublet(String name) {
-        return this.doubletDigger.isDoublet(name);
-	}
-
-	/**
-	 * Returns the first doublet of the given classname or resource.
-	 *
-	 * @param name
-	 *            a classname or resource
-	 * @return the first doublet
-	 */
-	@Override
-	@Description("returns the first doublet of the given classname or resource")
-	public URI getFirstDoublet(String name) {
-		return this.getDoublet(name, 1);
-	}
-
-	/**
-	 * Gets the doublet.
-	 *
-	 * @param name
-	 *            the name
-	 * @param nr
-	 *            the nr
-	 * @return the doublet
-	 */
-	@Override
-	public URI getDoublet(final String name, final int nr) {
-		return this.doubletDigger.getDoublet(name, nr);
-	}
-
-	/**
-	 * Gets the doublets.
-     * <p>
-     * Note: The Manifest file (META-INF/MANIFEST.MF) ist not counted as
-     * doublet file because it is not really a resource. 
-     * </p>
-	 *
-	 * @return the doublets
-	 */
-	@Override
-	public String[] getDoublets() {
-		List<String> doublets = new ArrayList<>();
-		for (String resource : this.getResources()) {
-			try {
-				if (!"/META-INF/MANIFEST.MF".equals(resource) && this.isDoublet(resource)) {
-					doublets.add(resource);
-				}
-			} catch (NoSuchElementException ex) {
-				LOG.debug("'{}' is ignored as resource ({})", resource, ex.getMessage());
-				LOG.trace("Details:", ex);
-			}
-		}
-		return doublets.toArray(new String[doublets.size()]);
-	}
-
-	/**
-	 * Looks for each found doublet resource in which classpath it was found.
-	 * Resources like META-INF/MANIFEST.MF are not considered as doublet -
-	 * otherwise you'll find all classpathes in the array.
-	 *
-	 * @return the classpath where doublets were found
-	 */
-	@Override
-	@Description("returns the classpath where doublets were found")
-	public String[] getDoubletResourcepath() {
-		URI[] resourcepathURIs = this.getDoubletResourcepathURIs();
-		return toStringArray(resourcepathURIs);
-	}
-
-	/**
-	 * Gets the URIs of the doublet resourcepath. Doublets in the
-	 * META-INF folder are filtered out because these resources
-	 * contains often some meta information of the JAR.
-	 *
-	 * @return the URIs of the doublets
-	 */
-	public URI[] getDoubletResourcepathURIs() {
-		LOG.trace("Calculating doublet-resourcepath.");
-		List<String> doubletsWithoutMetaInf = new ArrayList<>();
-		for (String doublet : this.getDoublets()) {
-			if (!"/META-INF/MANIFEST.MF".equals(doublet)) {
-				doubletsWithoutMetaInf.add(doublet);
-			}
-		}
-		Set<URI> resourcepathSet = this.resourcepathDigger.getResourcepathSet(doubletsWithoutMetaInf);
-		return resourcepathSet.toArray(new URI[resourcepathSet.size()]);
-	}
-
-	/**
-	 * Incompatible resources are doublets with different content.
-	 *
-	 * @return doublet resources with different content
-	 */
-	@Override
-	public String[] getIncompatibleResources() {
-		List<String> incompatibleResources = new ArrayList<>();
-		for (String rsc : this.getDoublets()) {
-			Enumeration<URL> doubletURLs = this.getResourcesFor(rsc);
-			try {
-				URL url = doubletURLs.nextElement();
-				ArchivEntry archivEntry = new ArchivEntry(url);
-				while (doubletURLs.hasMoreElements()) {
-					url = doubletURLs.nextElement();
-					ArchivEntry doubletEntry = new ArchivEntry(url);
-					if (archivEntry.equals(doubletEntry)) {
-						incompatibleResources.add(rsc);
-						break;
-					}
-				}
-			} catch (NoSuchElementException nse) {
-				LOG.warn("{} is not added to incompatible resource list:", rsc, nse);
-			}
-		}
-		return incompatibleResources.toArray(new String[incompatibleResources.size()]);
-	}
-
-	/**
-	 * Gets the classpath which contains incompatible resources. Resources in
-	 * the META-INF folder are filtered out because resources in this folder
-	 * contains often only some meta info of the JAR.
-	 *
-	 * @return the classpathes where incompatible resources were found
-	 */
-	@Override
-	public String[] getIncompatibleResourcepath() {
-        URI[] resourcepathURIs = this.getIncompatibleResourcepathURIs();
-        return toStringArray(resourcepathURIs);
-	}
-	
     /**
      * Gets the classpath which contains incompatible resources. Resources in
      * the META-INF folder are filtered out because resources in this folder
@@ -341,45 +94,9 @@ public class ResourcepathMonitor extends clazzfish.monitor.AbstractMonitor imple
      *
      * @return the resourcepath as array of URIs
      */
+    @Override
 	public URI[] getIncompatibleResourcepathURIs() {
-        List<String> resourcesWithoutMetainf = new ArrayList<String>();
-        for (String resource : this.getIncompatibleResources()) {
-            if (!resource.startsWith("/META-INF")) {
-                resourcesWithoutMetainf.add(resource);
-            }
-        }
-        Set<URI> resourcepathSet = this.resourcepathDigger.getResourcepathSet(resourcesWithoutMetainf);
-        return resourcepathSet.toArray(new URI[resourcepathSet.size()]);
-	}
-
-	/**
-	 * Logs the different array to the log output.
-	 */
-	@Override
-	public void logMe() {
-		try {
-			StringWriter writer = new StringWriter();
-			dumpArray(this.getResources(), new BufferedWriter(writer), "resources");
-			LOG.info(writer.toString());
-		} catch (IOException cannothappen) {
-			LOG.warn("Cannot dump resources:", cannothappen);
-		}
-	}
-
-	/**
-	 * This operation dumps the different MBean attributes to the given
-	 * directory.
-	 *
-	 * @param dumpDir
-	 *            the directory where the attributes are dumped to.
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	@Override
-	public void dumpMe(final File dumpDir) throws IOException {
-        this.dump(dumpDir, "DoubletResourcepath", "DoubletResourcepathURIs", "Doublets", "IncompatibleResources",
-                "IncompatibleResourcepath", "IncompatibleResourcepathURIs", "Resources");
-        copyResource("RscMonREADME.txt", new File(dumpDir, "README.txt"));
+		return super.getIncompatibleResourcepathURIs();
 	}
 
 }
