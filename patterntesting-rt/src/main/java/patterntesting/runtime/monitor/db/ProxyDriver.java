213aae17091ef832bab0444c78fd952679e2669b
oboehm <o.boehm@optica.de>
2019-01-07 08:03:27 +0100

    ProxyDriver marked as deprecated

--- patterntesting-rt/src/main/java/patterntesting/runtime/monitor/db/ProxyDriver.java
+++ patterntesting-rt/src/main/java/patterntesting/runtime/monitor/db/ProxyDriver.java
@@ -20,12 +20,9 @@
 
 package patterntesting.runtime.monitor.db;
 
-import java.sql.*;
-import java.util.*;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.*;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * This JDBC driver acts like a proxy between PatternTesting and the real JDBC
@@ -41,26 +38,10 @@ import org.apache.logging.log4j.*;
  * @author oliver
  * @version $Revision: 1.10 $
  * @since 1.4.1 (27.03.2014)
+ * @deprecated since 2.0, use {@link clazzfish.jdbc.ProxyDriver}
  */
-public class ProxyDriver implements Driver {
-
-	private static final String JDBC_URL_PREFIX = "jdbc:proxy:";
-	private static final Logger LOG = LogManager.getLogger(ProxyDriver.class);
-	private static final Map<String, String> KNOWN_DRIVERS = new HashMap<>();
-
-	/** Register class as JDBC driver. */
-	static {
-		register();
-		KNOWN_DRIVERS.put("hsqldb", "org.hsqldb.jdbc.JDBCDriver");
-		KNOWN_DRIVERS.put("informix-sqli", "com.informix.jdbc.IfxDriver");
-		KNOWN_DRIVERS.put("jturbo", "com.newatlanta.jturbo.driver.Driver");
-		KNOWN_DRIVERS.put("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	}
-
-	private static String getKnownDriverFor(final String jdbcURL) {
-		String[] parts = StringUtils.split(jdbcURL + ":x", ':');
-		return KNOWN_DRIVERS.get(parts[1].toLowerCase());
-	}
+@Deprecated
+public class ProxyDriver extends clazzfish.jdbc.ProxyDriver implements Driver {
 
 	/**
 	 * Registers the driver as JDBC driver.
@@ -79,45 +60,7 @@ public class ProxyDriver implements Driver {
 	 * </p>
 	 */
 	public static void register() {
-		Driver driver = new ProxyDriver();
-		try {
-			Set<Driver> deregistered = deregisterDrivers();
-			DriverManager.registerDriver(driver);
-			registerDrivers(deregistered);
-			LOG.debug("{} successful registered as JDBC driver.", driver);
-		} catch (SQLException ex) {
-			DriverManager.println("Cannot register " + driver + " because of " + ex.getMessage() + ".");
-			LOG.error("Cannot register {} as JDBC driver.", driver, ex);
-		}
-	}
-
-	private static Set<Driver> deregisterDrivers() {
-		Set<Driver> deregistered = new HashSet<>();
-		Enumeration<Driver> registered = DriverManager.getDrivers();
-		while (registered.hasMoreElements()) {
-			deregistered.add(registered.nextElement());
-		}
-		for (Driver driver : deregistered) {
-			try {
-				DriverManager.deregisterDriver(driver);
-			} catch (SQLException sex) {
-				LOG.debug("Cannot deregister {} from DriverManager.", driver);
-				LOG.trace("Details: ", sex);
-				deregistered.remove(driver);
-			}
-		}
-		LOG.trace("{} driver(s) deregistered.", deregistered.size());
-		return deregistered;
-	}
-
-	private static void registerDrivers(final Set<Driver> deregistered) {
-		for (Driver driver : deregistered) {
-			try {
-				DriverManager.registerDriver(driver);
-			} catch (SQLException sex) {
-				LOG.warn("Cannot register {} again:", driver, sex);
-			}
-		}
+		clazzfish.jdbc.ProxyDriver.register();
 	}
 
 	/**
@@ -128,11 +71,7 @@ public class ProxyDriver implements Driver {
 	 * @return the real driver name
 	 */
 	public static String getRealURL(final String jdbcURL) {
-		if (jdbcURL.startsWith(JDBC_URL_PREFIX)) {
-			return "jdbc:" + StringUtils.substring(jdbcURL, JDBC_URL_PREFIX.length());
-		} else {
-			return jdbcURL;
-		}
+		return clazzfish.jdbc.ProxyDriver.getRealURL(jdbcURL);
 	}
 
 	/**
@@ -143,15 +82,7 @@ public class ProxyDriver implements Driver {
 	 * @return the real driver name
 	 */
 	public static String getRealDriverName(final String jdbcURL) {
-		return getDriverName(getRealURL(jdbcURL));
-	}
-
-	private static String getDriverName(final String jdbcURL) {
-		String driverName = getKnownDriverFor(jdbcURL);
-		if (driverName == null) {
-			return getDriver(jdbcURL).getClass().getName();
-		}
-		return driverName;
+		return clazzfish.jdbc.ProxyDriver.getRealDriverName(jdbcURL);
 	}
 
 	/**
@@ -162,161 +93,7 @@ public class ProxyDriver implements Driver {
 	 * @return the real driver
 	 */
 	public static Driver getRealDriver(final String jdbcURL) {
-		String realURL = getRealURL(jdbcURL);
-		return getDriver(realURL);
-	}
-
-	private static Driver getDriver(final String url) {
-		try {
-			return DriverManager.getDriver(url);
-		} catch (SQLException ex) {
-			LOG.trace("Cannot get driver from DriverManager.", ex);
-			LOG.debug("Must first load driver for \"{}\" because {}.", url, ex.getMessage());
-			return loadDriverFor(url);
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	private static Driver loadDriverFor(final String jdbcURL) {
-		String driverName = getKnownDriverFor(jdbcURL);
-		try {
-			if (driverName != null) {
-				Class<? extends Driver> driverClass = (Class<? extends Driver>) Class.forName(driverName);
-				LOG.debug("Driver {} for URL \"{}\" loaded.", driverName, jdbcURL);
-				Driver driver = driverClass.newInstance();
-				DriverManager.registerDriver(driver);
-				LOG.debug("Driver {} for URL \"{}\" registered.", driver, jdbcURL);
-			}
-			return DriverManager.getDriver(jdbcURL);
-		} catch (SQLException ex) {
-			throw new IllegalArgumentException("unregistered URL: \"" + jdbcURL + '"', ex);
-		} catch (ClassNotFoundException ex) {
-			throw new IllegalArgumentException("cannot load driver for \"" + jdbcURL + '"', ex);
-		} catch (ReflectiveOperationException ex) {
-			throw new IllegalArgumentException(
-					"driver '" + driverName + "' for URL \"{}\" loaded but registration failed", ex);
-		}
-	}
-
-	/**
-	 * Retrieves whether the driver thinks that it can open a connection to the
-	 * given URL. Accepted URLs are URLs beginning with:
-	 * <ul>
-	 * <li><tt>jdbc:proxy:</tt>...</li>
-	 * <li><tt>jdbc:jamon:</tt>... (if JAMon is in the classpath)</li>
-	 * </ul>
-	 *
-	 * @param url
-	 *            the JDBC URL
-	 * @return true, if successful
-	 * @see Driver#acceptsURL(String)
-	 */
-	@Override
-	public boolean acceptsURL(final String url) {
-		String prefix = url.toLowerCase();
-		return StringUtils.startsWith(prefix, JDBC_URL_PREFIX);
-	}
-
-	/**
-	 * Attempts to make a database connection to the given URL. The driver
-	 * returns "null" if it realizes it is the wrong kind of driver to connect
-	 * to the given URL. This will be common, as when the JDBC driver manager is
-	 * asked to connect to a given URL it passes the URL to each loaded driver
-	 * in turn.
-	 *
-	 * @param url
-	 *            the url
-	 * @param info
-	 *            the info (e.g. user/password)
-	 * @return the connection
-	 * @throws SQLException
-	 *             the sQL exception
-	 * @see Driver#connect(String, Properties)
-	 */
-	@Override
-	public Connection connect(final String url, final Properties info) throws SQLException {
-		LOG.trace("Connecting to URL \"{}\"...", url);
-		if (!acceptsURL(url)) {
-			LOG.trace("{} does not accept \"{}\" as URL.", this, url);
-			return null;
-		}
-		String realURL = getRealURL(url);
-		Driver realDriver = getDriver(realURL);
-		Connection connection = realDriver.connect(realURL, info);
-		LOG.trace("Connected to real URL \"{}\".", realURL);
-		return ProxyConnection.newInstance(connection);
-	}
-
-	/**
-	 * Gets the major version.
-	 *
-	 * @return major version
-	 */
-	@Override
-	public int getMajorVersion() {
-		return 1;
-	}
-
-	/**
-	 * Gets the minor version.
-	 *
-	 * @return the minor version
-	 */
-	@Override
-	public int getMinorVersion() {
-		return 4;
-	}
-
-	/**
-	 * Gets the property info.
-	 *
-	 * @param url
-	 *            the url
-	 * @param info
-	 *            the info
-	 * @return the property info
-	 * @throws SQLException
-	 *             the SQL exception
-	 */
-	@Override
-	public DriverPropertyInfo[] getPropertyInfo(final String url, final Properties info) throws SQLException {
-		String realURL = getRealURL(url);
-		Driver driver = getDriver(realURL);
-		return driver.getPropertyInfo(realURL, info);
-	}
-
-	/**
-	 * Jdbc compliant.
-	 *
-	 * @return true, if successful
-	 */
-	@Override
-	public boolean jdbcCompliant() {
-		return true;
-	}
-
-	/**
-	 * Gets the parent logger. This method is needed for the support of Java 5.
-	 *
-	 * @return the parent logger
-	 * @throws SQLFeatureNotSupportedException
-	 *             the SQL feature not supported exception
-	 */
-	@Override
-	public java.util.logging.Logger getParentLogger() throws SQLFeatureNotSupportedException {
-		throw new SQLFeatureNotSupportedException("not yet implemented");
-	}
-
-	/**
-	 * Better toString implementation which supports logging and debugging.
-	 *
-	 * @return the string
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return this.getClass().getSimpleName() + " " + this.getMajorVersion() + "." + this.getMinorVersion() + " for \""
-				+ JDBC_URL_PREFIX + "...\"";
+		return clazzfish.jdbc.ProxyDriver.getRealDriver(jdbcURL);
 	}
 
 }
