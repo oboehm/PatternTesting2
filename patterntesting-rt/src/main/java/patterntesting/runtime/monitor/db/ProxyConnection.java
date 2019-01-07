07a65ae146ed96a842b19e1d14efbac5321482f9
oboehm <o.boehm@optica.de>
2019-01-07 07:53:52 +0100

    ProxyConnection marked as deprecated

--- patterntesting-rt/src/main/java/patterntesting/runtime/monitor/db/ProxyConnection.java
+++ patterntesting-rt/src/main/java/patterntesting/runtime/monitor/db/ProxyConnection.java
@@ -20,17 +20,10 @@
 
 package patterntesting.runtime.monitor.db;
 
-import java.lang.reflect.*;
-import java.sql.*;
-import java.util.Collection;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.*;
-
 import patterntesting.runtime.annotation.IgnoreForSequenceDiagram;
-import patterntesting.runtime.monitor.db.internal.StasiPreparedStatement;
-import patterntesting.runtime.monitor.db.internal.StasiStatement;
+
+import java.lang.reflect.InvocationHandler;
+import java.sql.Connection;
 
 /**
  * This is a dynamic proxy for a JDBC connection which monitors together with
@@ -43,16 +36,11 @@ import patterntesting.runtime.monitor.db.internal.StasiStatement;
  * @author oliver (ob@aosd.de)
  * @version $Revision: 1.18 $
  * @since 1.3 (29.09.2012)
+ * @deprecated since 2.0, use {@link clazzfish.jdbc.ProxyConnection}
  */
+@Deprecated
 @IgnoreForSequenceDiagram
-public class ProxyConnection implements InvocationHandler {
-
-	private static final Logger LOG = LogManager.getLogger(ProxyConnection.class);
-	private final Connection connection;
-	private final StackTraceElement[] caller;
-	private final Collection<StasiStatement> uncommittedStatements = new CopyOnWriteArrayList<>();
-	private boolean committed = false;
-	private boolean autoCommit = true;
+public class ProxyConnection extends clazzfish.jdbc.ProxyConnection implements InvocationHandler {
 
 	/**
 	 * Creates a new proxy instance for the given connection.
@@ -62,10 +50,7 @@ public class ProxyConnection implements InvocationHandler {
 	 * @return the connection
 	 */
 	public static Connection newInstance(final Connection connection) {
-		ProxyConnection proxyConnection = new ProxyConnection(connection);
-		ConnectionMonitor.addConnection(proxyConnection);
-		Class<?>[] interfaces = new Class[] { Connection.class };
-		return (Connection) Proxy.newProxyInstance(connection.getClass().getClassLoader(), interfaces, proxyConnection);
+		return clazzfish.jdbc.ProxyConnection.newInstance(connection);
 	}
 
 	/**
@@ -79,128 +64,7 @@ public class ProxyConnection implements InvocationHandler {
 	 *            the connection
 	 */
 	protected ProxyConnection(final Connection connection) {
-		this.connection = connection;
-		this.caller = StasiStatement.getCallerStacktrace(ProxyConnection.class, ProxyDriver.class,
-				ConnectionMonitor.class, DriverManager.class);
-		try {
-			this.autoCommit = connection.getAutoCommit();
-		} catch (SQLException sex) {
-			LOG.debug("Cannot decide if auto-commit is on:", sex);
-		}
+		super(connection);
 	}
-
-	/**
-	 * Invokes the orginal {@link Connection} method and puts a wrapper around
-	 * {@link Statement} and {@link PreparedStatement} to support monitoring.
-	 * <p>
-	 * TODO: CallableStatement is not yet wrapped.
-	 * </p>
-	 *
-	 * @param proxy
-	 *            the proxy
-	 * @param method
-	 *            the method
-	 * @param args
-	 *            the args
-	 * @return the object
-	 * @throws Throwable
-	 *             the throwable
-	 * @see java.lang.reflect.InvocationHandler#invoke(Object, Method, Object[])
-	 */
-	@Override
-	public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-		LOG.trace("Call of {} is delegated to {}.", proxy, connection);
-		String methodName = method.getName();
-		try {
-			if ("createStatement".equals(methodName)) {
-				Statement stmt = (Statement) method.invoke(connection, args);
-				return save(new StasiStatement(stmt));
-			} else if ("prepareStatement".equals(methodName)) {
-				PreparedStatement stmt = (PreparedStatement) method.invoke(connection, args);
-				return save(new StasiPreparedStatement(stmt, args));
-			} else if ("toString".equals(methodName)) {
-				return this.toString();
-			}
-			return invoke(method, args);
-		} catch (InvocationTargetException ex) {
-			Throwable cause = ex.getTargetException();
-			LOG.trace("Cannot invoke {}:", method, ex);
-			throw cause;
-		}
-	}
-
-	private Object invoke(final Method method, final Object[] args) throws Throwable {
-		String methodName = method.getName();
-		if ("close".equals(methodName)) {
-			this.close();
-		} else if ("setAutoCommit".equals(methodName)) {
-			this.autoCommit = (Boolean) args[0];
-		} else if (isCommitMethod(methodName)) {
-			commit();
-		}
-		return method.invoke(connection, args);
-	}
-
-	private void commit() {
-		this.committed = true;
-		this.uncommittedStatements.clear();
-	}
-
-	private boolean isCommitMethod(String methodName) {
-		return "commit".equals(methodName) || "rollback".equals(methodName);
-	}
-
-	private void close() {
-		ConnectionMonitor.removeConnection(this);
-		for (StasiStatement stmt : this.uncommittedStatements) {
-			int updateCount = stmt.getUpdateCount();
-			if ((updateCount > 0) && !this.isCommitted()) {
-				LOG.warn("{} entries were updated with '{}' but not committed.", updateCount, stmt);
-			}
-		}
-		LOG.trace("'{}' is closed, {} statement(s) will be freed.", this, this.uncommittedStatements.size());
-		this.uncommittedStatements.clear();
-	}
-
-	private Statement save(StasiStatement stmt) {
-		if (!this.autoCommit) {
-			this.uncommittedStatements.add(stmt);
-			LOG.trace("{} is added to {}.", stmt, this);
-		}
-		return stmt;
-	}
-
-	private boolean isCommitted() {
-		return this.autoCommit || this.committed;
-	}
-
-	/**
-	 * Gets the wrapped connection.
-	 *
-	 * @return the connection
-	 */
-	public Connection getConnection() {
-		return this.connection;
-	}
-
-	/**
-	 * Gets the stacktrace of the caller.
-	 *
-	 * @return the caller stacktrace
-	 */
-	public StackTraceElement[] getCaller() {
-		return this.caller;
-	}
-
-	/**
-	 * To string.
-	 *
-	 * @return the string
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return this.getClass().getSimpleName() + " for " + this.caller[0];
-	}
-
+	
 }
